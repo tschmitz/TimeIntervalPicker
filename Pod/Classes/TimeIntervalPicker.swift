4a8e69ae5e6fa1faf022eb97c7772c79e988e8ae
Tim Schmitz <tim@tapandtonic.net>
2017-03-10 21:26:43 -0500

    Update to Swift 3

--- Pod/Classes/TimeIntervalPicker.swift
+++ Pod/Classes/TimeIntervalPicker.swift
@@ -8,8 +8,8 @@ private let hoursInDay = 24
 internal class DigitsLabel: UIView {
     internal var text: String = "" { didSet { label.text = text } }
     
-    private let textAlignment = NSTextAlignment.Right
-    private var label: UILabel!
+    fileprivate let textAlignment = NSTextAlignment.right
+    fileprivate var label: UILabel!
     
     internal init(width: CGFloat, height: CGFloat, labelWidth: CGFloat, font: UIFont, textColor: UIColor) {
         super.init(frame: CGRect(x: 0, y: 0, width: width, height: height))
@@ -20,7 +20,7 @@ internal class DigitsLabel: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func createLabel(width width: CGFloat, height: CGFloat, font: UIFont, textColor: UIColor) {
+    fileprivate func createLabel(width: CGFloat, height: CGFloat, font: UIFont, textColor: UIColor) {
         label = UILabel(frame: CGRect(x: 0, y: 0, width: width, height: height))
         addSubview(label)
         label.textAlignment = textAlignment
@@ -31,69 +31,69 @@ internal class DigitsLabel: UIView {
     
 }
 
-public class TimeIntervalPicker: UIControl, UIPickerViewDataSource, UIPickerViewDelegate {
+open class TimeIntervalPicker: UIControl, UIPickerViewDataSource, UIPickerViewDelegate {
     
     // MARK: Value access
     
     /// Value indicated by the picker in seconds
-    public var countDownDuration: NSTimeInterval {
+    open var countDownDuration: TimeInterval {
         get {
-            let secondsFromHoursComponent = pickerView.selectedRowInComponent(Components.Hour.rawValue) * secondsInHour
-            let secondsFromMinutesComponent = pickerView.selectedRowInComponent(Components.Minute.rawValue) % minutesInHour * secondsInMinute
-            return NSTimeInterval(secondsFromHoursComponent + secondsFromMinutesComponent)
+            let secondsFromHoursComponent = pickerView.selectedRow(inComponent: Components.hour.rawValue) * secondsInHour
+            let secondsFromMinutesComponent = pickerView.selectedRow(inComponent: Components.minute.rawValue) % minutesInHour * secondsInMinute
+            return TimeInterval(secondsFromHoursComponent + secondsFromMinutesComponent)
         }
         set(value) {
             let hours = Int(value) / secondsInHour
             let minutes = (Int(value) - hours * secondsInHour) / secondsInMinute
             
-            pickerView.selectRow(hours % hoursInDay, inComponent: Components.Hour.rawValue, animated: false)
-            pickerView.selectRow(minuteRowsCount / 2 + minutes, inComponent: Components.Minute.rawValue, animated: false)
+            pickerView.selectRow(hours % hoursInDay, inComponent: Components.hour.rawValue, animated: false)
+            pickerView.selectRow(minuteRowsCount / 2 + minutes, inComponent: Components.minute.rawValue, animated: false)
         }
     }
     
-    public var date: NSDate {
+    open var date: Date {
         get {
-            let components = NSDateComponents()
+            var components = DateComponents()
             components.second = Int(countDownDuration)
-            return NSCalendar.currentCalendar().dateFromComponents(components)!
+            return Calendar.current.date(from: components)!
         }
         set(newDate) {
-            let components = NSCalendar.currentCalendar().components([.Hour, .Minute, .Second], fromDate: newDate)
-            countDownDuration = NSTimeInterval(components.hour * 3600 + components.minute * 60 + components.second)
+            let components = (Calendar.current as NSCalendar).components([.hour, .minute, .second], from: newDate)
+            countDownDuration = TimeInterval(components.hour! * 3600 + components.minute! * 60 + components.second!)
         }
     }
     
-    public func setDate(newDate: NSDate, animated: Bool) {
+    open func setDate(_ newDate: Date, animated: Bool) {
         // TODO: implement animation
         date = newDate
     }
     
-    public var datePickerMode: UIDatePickerMode {
-        get { return .CountDownTimer }
-        set(newMode) { assert(newMode == .CountDownTimer) }
+    open var datePickerMode: UIDatePickerMode {
+        get { return .countDownTimer }
+        set(newMode) { assert(newMode == .countDownTimer) }
     }
     
     // MARK: Layout and geometry
     // The defaults values aim to resemble the look of UIDataPicker
     
     /// Width of a picker component
-    public var componentWidth: CGFloat = 102
+    open var componentWidth: CGFloat = 102
     
     /// Size of a label that shows hours/minutes digits within a component
-    public var digitsLabelSize = CGSize(width: 30, height: 30)
+    open var digitsLabelSize = CGSize(width: 30, height: 30)
     
     /// Font of labels that show hours/minutes digits within a component
-    public var digitsLabelFont = UIFont.systemFontOfSize(23.5) {
+    open var digitsLabelFont = UIFont.systemFont(ofSize: 23.5) {
         didSet { setNeedsDisplay() }
     }
     
     /// Text color of labels that show hours/minutes digits within a component
-    public var digitsLabelTextColor = UIColor.blackColor() {
+    open var digitsLabelTextColor = UIColor.black {
         didSet { setNeedsDisplay() }
     }
     
     /// Font of "hours" and "min" labels
-    public var minutesHoursLabelFont = UIFont.boldSystemFontOfSize(17) {
+    open var minutesHoursLabelFont = UIFont.boldSystemFont(ofSize: 17) {
         didSet {
             minutesFloatingLabel.font = minutesHoursLabelFont
             hoursFloatingLabel.font = minutesHoursLabelFont
@@ -101,7 +101,7 @@ public class TimeIntervalPicker: UIControl, UIPickerViewDataSource, UIPickerView
     }
     
     /// Text color of "hours" and "min" labels
-    public var minutesHoursLabelTextColor = UIColor.blackColor() {
+    open var minutesHoursLabelTextColor = UIColor.black {
         didSet {
             minutesFloatingLabel.textColor = minutesHoursLabelTextColor
             hoursFloatingLabel.textColor = minutesHoursLabelTextColor
@@ -110,17 +110,17 @@ public class TimeIntervalPicker: UIControl, UIPickerViewDataSource, UIPickerView
     
     // MARK: Private details
     
-    private let componentsNumber = 2
+    fileprivate let componentsNumber = 2
     
-    private enum Components: Int {
-        case Hour = 0
-        case Minute = 1
+    fileprivate enum Components: Int {
+        case hour = 0
+        case minute = 1
     }
     
-    private let minuteRowsCount = minutesInHour * 1000
-    private var pickerView: UIPickerView!
-    private var hoursFloatingLabel: UILabel!
-    private var minutesFloatingLabel: UILabel!
+    fileprivate let minuteRowsCount = minutesInHour * 1000
+    fileprivate var pickerView: UIPickerView!
+    fileprivate var hoursFloatingLabel: UILabel!
+    fileprivate var minutesFloatingLabel: UILabel!
     
     // MARK: Initializers
     
@@ -129,11 +129,11 @@ public class TimeIntervalPicker: UIControl, UIPickerViewDataSource, UIPickerView
         
         createPickerView()
         createFloatingLabels()
-        autoresizingMask = [.FlexibleHeight, .FlexibleWidth]
+        autoresizingMask = [.flexibleHeight, .flexibleWidth]
         
         // Creates an illusion of an infinitly-looped minute: selector
         let middleMinutesRow = minuteRowsCount / 2
-        pickerView.selectRow(middleMinutesRow, inComponent: Components.Minute.rawValue, animated: false)
+        pickerView.selectRow(middleMinutesRow, inComponent: Components.minute.rawValue, animated: false)
     }
     
     required public init?(coder aDecoder: NSCoder) {
@@ -141,14 +141,14 @@ public class TimeIntervalPicker: UIControl, UIPickerViewDataSource, UIPickerView
         
         createPickerView()
         createFloatingLabels()
-        autoresizingMask = [.FlexibleHeight, .FlexibleWidth]
+        autoresizingMask = [.flexibleHeight, .flexibleWidth]
         
         // Creates an illusion of an infinitly-looped minute: selector
         let middleMinutesRow = minuteRowsCount / 2
-        pickerView.selectRow(middleMinutesRow, inComponent: Components.Minute.rawValue, animated: false)
+        pickerView.selectRow(middleMinutesRow, inComponent: Components.minute.rawValue, animated: false)
     }
     
-    private func createPickerView() {
+    fileprivate func createPickerView() {
         pickerView = UIPickerView()
         pickerView.delegate = self
         pickerView.dataSource = self
@@ -158,37 +158,37 @@ public class TimeIntervalPicker: UIControl, UIPickerViewDataSource, UIPickerView
         // Fill the whole container:
         let width = NSLayoutConstraint(
             item: pickerView,
-            attribute: NSLayoutAttribute.Width,
-            relatedBy: NSLayoutRelation.Equal,
+            attribute: NSLayoutAttribute.width,
+            relatedBy: NSLayoutRelation.equal,
             toItem: self,
-            attribute: NSLayoutAttribute.Width,
+            attribute: NSLayoutAttribute.width,
             multiplier: 1.0,
             constant: 0)
         
         let height = NSLayoutConstraint(
             item: pickerView,
-            attribute: NSLayoutAttribute.Height,
-            relatedBy: NSLayoutRelation.Equal,
+            attribute: NSLayoutAttribute.height,
+            relatedBy: NSLayoutRelation.equal,
             toItem: self,
-            attribute: NSLayoutAttribute.Height,
+            attribute: NSLayoutAttribute.height,
             multiplier: 1.0,
             constant: 0)
         
         let top = NSLayoutConstraint(
             item: pickerView,
-            attribute:NSLayoutAttribute.Top,
-            relatedBy:NSLayoutRelation.Equal,
+            attribute:NSLayoutAttribute.top,
+            relatedBy:NSLayoutRelation.equal,
             toItem: self,
-            attribute: NSLayoutAttribute.Top,
+            attribute: NSLayoutAttribute.top,
             multiplier: 1.0,
             constant: 0)
         
         let leading = NSLayoutConstraint(
             item: pickerView,
-            attribute: NSLayoutAttribute.Leading,
-            relatedBy: NSLayoutRelation.Equal,
+            attribute: NSLayoutAttribute.leading,
+            relatedBy: NSLayoutRelation.equal,
             toItem: self,
-            attribute: NSLayoutAttribute.Leading,
+            attribute: NSLayoutAttribute.leading,
             multiplier: 1.0,
             constant: 0)
         
@@ -198,13 +198,13 @@ public class TimeIntervalPicker: UIControl, UIPickerViewDataSource, UIPickerView
         addConstraint(leading)
     }
     
-    private func createFloatingLabels() {
-        func createLabel(text: String) -> UILabel {
+    fileprivate func createFloatingLabels() {
+        func createLabel(_ text: String) -> UILabel {
             let label = UILabel()
             label.font = self.minutesHoursLabelFont
             label.text = text
             label.translatesAutoresizingMaskIntoConstraints = false
-            label.userInteractionEnabled = false
+            label.isUserInteractionEnabled = false
             label.adjustsFontSizeToFitWidth = false
             label.sizeToFit()
             return label
@@ -217,8 +217,8 @@ public class TimeIntervalPicker: UIControl, UIPickerViewDataSource, UIPickerView
         addSubview(minutesFloatingLabel)
     }
     
-    override public func layoutSubviews() {
-        func alignToBaselineOfSelectedRow(label: UILabel) {
+    override open func layoutSubviews() {
+        func alignToBaselineOfSelectedRow(_ label: UILabel) {
             let pickerViewMiddleY = pickerView.frame.origin.y + (pickerView.frame.height / 2)
             let digitsBaseline = pickerViewMiddleY + (digitsLabelFont.capHeight / 2)
             label.frame.origin.y = digitsBaseline - label.font.lineHeight - label.font.descender
@@ -241,44 +241,44 @@ public class TimeIntervalPicker: UIControl, UIPickerViewDataSource, UIPickerView
     
     // MARK: UIPickerViewDataSource methods
     
-    public func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+    open func numberOfComponents(in pickerView: UIPickerView) -> Int {
         assert(pickerView == self.pickerView)
         return componentsNumber
     }
     
-    public func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+    open func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         assert(pickerView == self.pickerView)
         switch Components(rawValue: component)! {
-        case Components.Hour:
+        case Components.hour:
             return hoursInDay
-        case Components.Minute:
+        case Components.minute:
             return minuteRowsCount // a high number to create an illusion of an infinitly-looped selector
         }
     }
     
     // MARK: UIPickerViewDelegate methods
     
-    public func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    open func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         assert(pickerView == self.pickerView)
-        sendActionsForControlEvents(UIControlEvents.ValueChanged)
+        sendActions(for: UIControlEvents.valueChanged)
     }
     
-    public func pickerView(pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+    open func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
         return componentWidth;
     }
     
-    public func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView {
+    open func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
         let label: DigitsLabel = view is DigitsLabel ? view as! DigitsLabel : DigitsLabel(width: componentWidth, height: digitsLabelSize.height, labelWidth: digitsLabelSize.width, font: digitsLabelFont, textColor: digitsLabelTextColor)
         label.text = self.pickerView(pickerView, titleForRow: row, forComponent: component)!
         return label
     }
     
-    public func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+    open func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         assert(pickerView == self.pickerView)
         switch Components(rawValue: component)! {
-        case Components.Hour:
+        case Components.hour:
             return row.description
-        case Components.Minute:
+        case Components.minute:
             return (row % minutesInHour).description
         }
     }
